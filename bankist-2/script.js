e7bc1884468d66cd40dfb1cf2bdd78169c07ecb8
Phosah <efosa.uyi@gmail.com>
2024-02-03 19:36:08 +0100

    add dynamic nav height

--- bankist-2/script.js
+++ bankist-2/script.js
@@ -143,6 +143,7 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 // observer.observe(section1);
 
 const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
 
 const stickyNav = function (entries) {
   const [entry] = entries;
@@ -155,7 +156,7 @@ const stickyNav = function (entries) {
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
-  rootMargin: '-90px',
+  rootMargin: `-${navHeight}px`,
 });
 
 headerObserver.observe(header);
